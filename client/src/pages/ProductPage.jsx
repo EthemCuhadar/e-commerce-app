8b64f1571fa0407fe69cad3d883c818dae603fb8
EthemCuhadar <iethemcuhadar@gmail.com>
2022-08-16 17:15:53 +0300

    Product page created

---
+++ client/src/pages/ProductPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Announcements from '../components/Announcements'
+import Products from '../components/Products'
+import NavbarProduct from '../components/NavbarProduct'
+
+const ProductsPage = () => {
+  
+  return (
+    <div>
+        <NavbarProduct/>
+        <Products/>
+    </div>
+  )
+}
+
+export default ProductsPage
