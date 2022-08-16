7ca7cc180718d2526d2a2cd60232012bfebdab29
EthemCuhadar <iethemcuhadar@gmail.com>
2022-08-16 16:01:13 +0300

    Mutation for login created

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -68,4 +68,10 @@ const LoginPage = () => {
   )
 }
 
+const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password)
+    }
+`
+
 export default LoginPage
