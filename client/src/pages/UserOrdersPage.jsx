95bd0b73d42b96959166cf8dd8380f648dfa5494
EthemCuhadar <iethemcuhadar@gmail.com>
2022-08-16 16:36:00 +0300

    User orders page created

---
+++ client/src/pages/UserOrdersPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavbarWelcome from '../components/NavbarWelcome'
+import UserOrders from '../components/UserOrders'
+
+const UserOrdersPage = () => {
+  return (
+    <div>
+        <NavbarWelcome />
+        <UserOrders />
+    </div>
+  )
+}
+
+export default UserOrdersPage
