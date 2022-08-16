ecba7cd299022e62f77cd5c4bc33ab3ed9b23d73
EthemCuhadar <iethemcuhadar@gmail.com>
2022-08-16 15:55:40 +0300

    Navbar for login page created

---
+++ client/src/components/NavbarLogin.jsx
@@ -0,0 +1,83 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+    height: 80px;
+    background-color: "white";
+    display: flex;
+    align-items: top;
+    justify-content: center;
+`
+
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 90%;
+    height: 60%;
+    background-color: Gainsboro;
+    border-radius: 5px;
+    margin-top: 10px;
+`
+
+const Right = styled.div`
+    flex: 1;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    flex-direction: row;
+    margin-right: 20px;
+    height: 20%;
+`
+
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: left;
+    height: 20%;
+`
+
+const Title = styled.h1`
+    font-size: 24px;
+    font-weight: 600;
+    text-align: left;
+    muted: true;
+    margin-left: 20px;
+    display: flex;
+    align-items: center;
+    margin-right: 20px;
+    text-decoration: none;
+    color: black;
+`
+
+const LinkItem = styled.a`
+    text-decoration: none;
+    color: black;
+    font-size: 18px;
+    font-weight: 500;
+    margin-left: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    &:hover {
+        color: red;
+    }
+`
+
+const NavbarWelcome = () => {
+  return (
+    <Container>
+        <Wrapper>
+            <Left>
+                <Title>E-commerce</Title>
+            </Left>
+            <Right>
+                <LinkItem href="/">Back</LinkItem>
+            </Right>
+        </Wrapper>
+    </Container>
+  )
+}
+
+export default NavbarWelcome
