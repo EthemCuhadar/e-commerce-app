8c232f6639c30782e1e16a33864447ecb41b1061
EthemCuhadar <iethemcuhadar@gmail.com>
2022-08-16 16:23:46 +0300

    Navbar for homepage created

---
+++ client/src/components/NavbarHome.jsx
@@ -0,0 +1,95 @@
+import styled from "styled-components"
+import { useNavigate } from "react-router-dom"
+
+const Container = styled.div`
+    height: 80px;
+    background-color: "white";
+    display: flex;
+    align-items: top;
+    justify-content: center;
+`
+
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 90%;
+    height: 60%;
+    background-color: Gainsboro;
+    border-radius: 5px;
+    margin-top: 10px;
+`
+
+const Right = styled.div`
+    flex: 1;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    flex-direction: row;
+    margin-right: 20px;
+    height: 20%;
+`
+
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: left;
+    height: 20%;
+`
+
+const Title = styled.h1`
+    font-size: 24px;
+    font-weight: 600;
+    text-align: left;
+    muted: true;
+    margin-left: 20px;
+    display: flex;
+    align-items: center;
+    margin-right: 20px;
+    text-decoration: none;
+    color: black;
+`
+
+const LinkItem = styled.a`
+    text-decoration: none;
+    color: black;
+    font-size: 18px;
+    font-weight: 500;
+    margin-left: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    &:hover {
+        color: red;
+    }
+`
+
+const NavbarProducts = () => {
+
+    const nav = useNavigate()
+
+    const handleLogout = () => {
+        localStorage.removeItem("token")
+        nav("/")
+    }
+
+  return (
+    <Container>
+        <Wrapper>
+            <Left>
+                <Title>E-commerce</Title>
+            </Left>
+            <Right>
+                <LinkItem href="/" onClick={handleLogout} >Logout</LinkItem>
+                <LinkItem href="/products">Products</LinkItem>
+                <LinkItem href="/orders">Orders</LinkItem>
+                <LinkItem href="/profile">Profile</LinkItem>
+            </Right>
+        </Wrapper>
+    </Container>
+  )
+}
+
+export default NavbarProducts
