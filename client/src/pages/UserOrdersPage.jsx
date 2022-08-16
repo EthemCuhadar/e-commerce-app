94436409f8968b730e5f79de3942882ed1d0ce88
EthemCuhadar <iethemcuhadar@gmail.com>
2022-08-16 17:01:47 +0300

    Navbar for orders page added

--- client/src/pages/UserOrdersPage.jsx
+++ client/src/pages/UserOrdersPage.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import NavbarWelcome from '../components/NavbarWelcome'
+import NavbarOrders from '../components/NavbarOrders'
 import UserOrderList from '../components/UserOrderList'
 
 const UserOrdersPage = () => {
   return (
     <div>
-        <NavbarWelcome />
+        <NavbarOrders />
         <UserOrderList />
     </div>
   )
