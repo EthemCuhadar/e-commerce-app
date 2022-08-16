5bc083d810e3c786051ce5f66ae58eb1fd223411
EthemCuhadar <iethemcuhadar@gmail.com>
2022-08-16 16:13:26 +0300

    Apollo client implemented

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+
+const createApolloClient = (token) => {
+  return new ApolloClient({
+    link: createHttpLink({
+      uri: 'https://simplisaleshw.cotunnel.com/graphql',
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    }),
+    cache: new InMemoryCache()
+  });
+}
+
+const client = createApolloClient(localStorage.getItem('token'));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
